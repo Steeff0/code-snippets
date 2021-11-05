72a81070de599785b43dc6b37870cf8f31d6ce0b
Steven Gerritsen <steven.gerritsen@gmail.com>
2021-11-05 14:26:10 +0100

    WIP chopper setup

---
+++ src/main/java/com/example/code_kata/kata02/Chopper.java
@@ -0,0 +1,5 @@
+package com.example.code_kata.kata02;
+
+public interface Chopper {
+    public int chop(int search, int[] numbers);
+}
---
+++ src/main/java/com/example/code_kata/kata02/ChopperImpl01.java
@@ -0,0 +1,14 @@
+package com.example.code_kata.kata02;
+
+public class ChopperImpl01 implements Chopper{
+
+    public int chop(int search, int[] numbers) {
+        if(numbers.length == 0){
+            return -1;
+        }
+
+
+
+        return -1;
+    }
+}
