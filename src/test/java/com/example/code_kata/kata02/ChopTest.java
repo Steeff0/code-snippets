af7e1332ace1930ab894482008d3c43b9987b917
Steven Gerritsen <steven.gerritsen@gmail.com>
2021-11-07 12:18:36 +0100

    Finish Unit test kata02

--- src/test/java/com/example/code_kata/kata02/ChopTest.java
+++ src/test/java/com/example/code_kata/kata02/ChopTest.java
@@ -1,52 +1,47 @@
 package com.example.code_kata.kata02;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class ChopTest{
 
-    Chopper chopper = new ChopperImpl01();
+    Chopper solution01 = new ChopperImpl01();
 
     static Stream<Arguments> intIntAndIntArrayProvider() {
         return Stream.of(
             arguments(-1, 3, new int[]{}),
-            arguments(-1, 3, new int[]{1})
+            arguments(-1, 3, new int[]{1}),
+            arguments(0,  1, new int[]{1}),
+            arguments(0,  1, new int[]{1, 3, 5}),
+            arguments(1,  3, new int[]{1, 3, 5}),
+            arguments(2,  5, new int[]{1, 3, 5}),
+            arguments(-1, 0, new int[]{1, 3, 5}),
+            arguments(-1, 2, new int[]{1, 3, 5}),
+            arguments(-1, 4, new int[]{1, 3, 5}),
+            arguments(-1, 6, new int[]{1, 3, 5}),
+            arguments(0,  1, new int[]{1, 3, 5, 7}),
+            arguments(1,  3, new int[]{1, 3, 5, 7}),
+            arguments(2,  5, new int[]{1, 3, 5, 7}),
+            arguments(3,  7, new int[]{1, 3, 5, 7}),
+            arguments(-1, 0, new int[]{1, 3, 5, 7}),
+            arguments(-1, 2, new int[]{1, 3, 5, 7}),
+            arguments(-1, 4, new int[]{1, 3, 5, 7}),
+            arguments(-1, 6, new int[]{1, 3, 5, 7}),
+            arguments(-1, 8, new int[]{1, 3, 5, 7})
         );
     }
 
     @DisplayName("Chopping gives correct result")
-    @ParameterizedTest(name = "{index} ==> {1} results in {0}")
+    @ParameterizedTest(name = "{index} ==> search {1} in {2} results in {0}")
     @MethodSource("intIntAndIntArrayProvider")
-    void testChopping(int result, int search, int[] set) {
-        assertEquals(result, chopper.chop(search, set));
-    }
-
-    @Test
-    void seachNumber() {
-        assertEquals(-1, chopper.chop(3, new int[]{}));
-        assertEquals(-1, chopper.chop(3, new int[]{1}));
-        assertEquals(1, chopper.chop(1, new int[]{1}));
-
-//        assert_equal(-1, chop(3, []))
-//        assert_equal(-1, chop(3, [1]))
-//        assert_equal(0,  chop(1, [1]))
-//        assert_equal(0,  chop(1, [1, 3, 5]))
-//        assert_equal(1,  chop(3, [1, 3, 5]))
-//        assert_equal(2,  chop(5, [1, 3, 5]))
-//        assert_equal(-1, chop(0, [1, 3, 5]))
-//        assert_equal(-1, chop(2, [1, 3, 5]))
-//        assert_equal(-1, chop(4, [1, 3, 5]))
-//        assert_equal(-1, chop(6, [1, 3, 5]))
-//        assert_equal(0,  chop(1, [1, 3, 5, 7]))
-//        assert_equal(1,  chop(3, [1, 3, 5, 7]))
-//        assert_equal(2,  chop(5, [1, 3, 5, 7]))
-//        assert_equal(3,  chop(7, [1, 3, 5, 7]))
-//        assert_equal(-1, chop(0, [1, 3, 5, 7]))
-//        assert_equal(-1, chop(2, [1, 3, 5, 7]))
-//        assert_equal(-1, chop(4, [1, 3, 5, 7]))
-//        assert_equal(-1, chop(6, [1, 3, 5, 7]))
-//        assert_equal(-1, chop(8, [1, 3, 5, 7]))
+    void testChoppingSolution01(int result, int search, int[] set) {
+        assertEquals(result, solution01.chop(search, set));
     }
 }
