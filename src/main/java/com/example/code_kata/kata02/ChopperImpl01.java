f3153a018e8b57d2212491752f82ab90bd82d566
Steven Gerritsen <steven.gerritsen@gmail.com>
2021-11-07 12:23:28 +0100

    Implement a poor man's solution

--- src/main/java/com/example/code_kata/kata02/ChopperImpl01.java
+++ src/main/java/com/example/code_kata/kata02/ChopperImpl01.java
@@ -1,14 +1,12 @@
 package com.example.code_kata.kata02;
 
 public class ChopperImpl01 implements Chopper{
-
     public int chop(int search, int[] numbers) {
-        if(numbers.length == 0){
-            return -1;
+        for(int i = 0; i < numbers.length; i++) {
+            if(numbers[i] == search) {
+                return i;
+            }
         }
-
-
-
         return -1;
     }
 }
